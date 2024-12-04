bd8902b08e9a811a24141caeac491e3a9490668e
Manisha <choudharymanisha38566@gmail.com>
2024-12-04 21:54:14 +0530

    security in springboot practice

---
+++ src/test/java/com/manisha/demo_sec/DemoSecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manisha.demo_sec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
